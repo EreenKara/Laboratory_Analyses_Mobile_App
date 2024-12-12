aaa6d54ddf012043ef2d5e873f63830ba1b932c7
Eren Kara <eren.kara2@ogr.sakarya.edu.tr>
2024-12-12 12:42:31 +0300

    minor updates

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { createStaticNavigation, NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './src/screens/Home';
-import UsersScreen from './src/screens/Users';
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import {
+  createStaticNavigation,
+  NavigationContainer,
+} from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./src/screens/Home";
+import UsersScreen from "./src/screens/Users";
 
 const Stack = createNativeStackNavigator();
 
@@ -13,8 +15,9 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Users" component={UsersScreen}/>
+        <Stack.Screen name="Users" component={UsersScreen} />
       </Stack.Navigator>
+      <Stack.Navigator></Stack.Navigator>
     </NavigationContainer>
   );
 }
@@ -22,8 +25,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
